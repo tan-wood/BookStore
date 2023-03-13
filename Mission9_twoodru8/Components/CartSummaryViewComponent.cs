153d85abc7ee304fcce3f7fa8d631cf427d053ff
tannerwoodrum <tannerwoodrum@TANNERWOODR8C05>
2023-03-13 10:31:27 -0600

    Adding font awesome and then adding the cart icon on the top that will show an account summary.

---
+++ Mission9_twoodru8/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission9_twoodru8.Model;
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart c)
+        {
+            cart = c;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
